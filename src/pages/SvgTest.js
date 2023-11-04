a7b407dd41690aac5d3bb4503fef7a1a5bcaca20
tkwk9 <tim.k.kwak@gmail.com>
2023-11-03 22:18:12 -0700

    million rects

--- src/pages/SvgTest.js
+++ src/pages/SvgTest.js
@@ -17,45 +17,56 @@ const SvgTest = () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
     container.appendChild(renderer.domElement);
 
+    const rectSize = 30;
+    const paddingSize = 10;
+    const comb = rectSize + paddingSize;
+
     const svgString = `
-    <svg width="5000" height="5000" xmlns="http://www.w3.org/2000/svg">
-      <rect width="100%" height="100%" fill="#3a3a3a" />
-
-      <defs>
-        <!-- Inner shadow filter definition -->
-        <filter id="inner-shadow" x="-50%" y="-50%" width="200%" height="200%">
-          <feComponentTransfer in="SourceAlpha">
-            <feFuncA type="table" tableValues="1 0" />
-          </feComponentTransfer>
-          <feGaussianBlur stdDeviation="2"/>
-          <feOffset dx="2" dy="2" result="offsetblur"/>
-          <feFlood flood-color="#000000" result="color"/>
-          <feComposite in2="offsetblur" operator="in"/>
-          <feComposite in2="SourceAlpha" operator="in" />
-          <feMerge>
-            <feMergeNode in="SourceGraphic" />
-            <feMergeNode />
-          </feMerge>
-        </filter>
-
-        <rect id="rounded-rect" width="40" height="40" rx="5" ry="5" style="fill:#9a9a9a; stroke: white; filter:url(#inner-shadow);"/>
-      </defs>
-      
-      <g fill="none" stroke="white">
-        ${Array.from(
-          { length: 100 },
-          (_, rowIndex) =>
-            `<!-- Row ${rowIndex + 1} -->
+    <svg width="${comb * 100}" height="${
+      comb * 100
+    }" xmlns="http://www.w3.org/2000/svg">
+    <!-- Padding around the entire SVG -->
+    <rect width="100%" height="100%" fill="#3a3a3a" />
+  
+    <defs>
+      <!-- Inner shadow filter definition -->
+      <filter id="inner-shadow" x="-50%" y="-50%" width="200%" height="200%">
+        <feComponentTransfer in="SourceAlpha">
+          <feFuncA type="table" tableValues="1 0" />
+        </feComponentTransfer>
+        <feGaussianBlur stdDeviation="2" />
+        <feOffset dx="2" dy="2" result="offsetblur" />
+        <feFlood flood-color="#000000" result="color" />
+        <feComposite in2="offsetblur" operator="in" />
+        <feComposite in2="SourceAlpha" operator="in" />
+        <feMerge>
+          <feMergeNode in="SourceGraphic" />
+          <feMergeNode />
+        </feMerge>
+      </filter>
+  
+      <rect id="rounded-rect" width="${rectSize}" height="${rectSize}" rx="5" ry="5" style="fill:#9a9a9a; stroke: white; filter:url(#inner-shadow);" />
+    </defs>
+  
+    <g fill="none" stroke="white">
+      ${Array.from(
+        { length: 100 },
+        (_, rowIndex) =>
+          `
             ${Array.from(
               { length: 100 },
               (_, colIndex) =>
-                `<use href="#rounded-rect" x="${colIndex * 50 + 5}" y="${
-                  rowIndex * 50 + 5
+                `<use href="#rounded-rect" x="${
+                  colIndex * (rectSize + paddingSize) + paddingSize / 2
+                }" y="${
+                  rowIndex * (rectSize + paddingSize) + paddingSize / 2
                 }" />`
             ).join("\n       ")}`
-        ).join("\n    ")}
-      </g>
-    </svg>
+      ).join("\n    ")}
+    </g>
+  </svg>
+  
+
     `;
 
     const svgBlob = new Blob([svgString], {
@@ -70,11 +81,21 @@ const SvgTest = () => {
         transparent: true,
       });
 
-      const geometry = new THREE.PlaneGeometry(10, 10);
+      const planeSize = 30;
+      const gridSize = 10;
 
-      const mesh = new THREE.Mesh(geometry, material);
-
-      scene.add(mesh);
+      for (let row = 0; row < gridSize; row++) {
+        for (let col = 0; col < gridSize; col++) {
+          const mesh = new THREE.Mesh(
+            new THREE.PlaneGeometry(planeSize, planeSize),
+            material
+          );
+          const xOffset = col * planeSize;
+          const yOffset = row * planeSize;
+          mesh.position.set(xOffset, -yOffset, 0);
+          scene.add(mesh);
+        }
+      }
 
       camera.position.z = 5;
 
@@ -90,28 +111,34 @@ const SvgTest = () => {
 
       const onMouseDown = (event) => {
         isPanning = true;
-        
-        mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-        mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-        
-        raycaster.setFromCamera( mouse, camera );
-        
-        raycaster.ray.intersectPlane(new THREE.Plane(new THREE.Vector3(0, 0, 1), 0), selectedPoint);
+
+        mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+        raycaster.setFromCamera(mouse, camera);
+
+        raycaster.ray.intersectPlane(
+          new THREE.Plane(new THREE.Vector3(0, 0, 1), 0),
+          selectedPoint
+        );
       };
 
       const onMouseMove = (event) => {
         if (!isPanning) return;
-        
-        mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-        mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-        
-        raycaster.setFromCamera( mouse, camera );
-        
+
+        mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+        raycaster.setFromCamera(mouse, camera);
+
         const newPoint = new THREE.Vector3();
-        raycaster.ray.intersectPlane(new THREE.Plane(new THREE.Vector3(0, 0, 1), 0), newPoint);
-        
+        raycaster.ray.intersectPlane(
+          new THREE.Plane(new THREE.Vector3(0, 0, 1), 0),
+          newPoint
+        );
+
         camera.position.sub(newPoint.sub(selectedPoint));
-        
+
         requestAnimationFrame(render);
       };
 
