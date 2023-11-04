b7f4a72906f4e833beac822a1997da65c42051b4
tkwk9 <tim.k.kwak@gmail.com>
2023-11-04 08:04:17 -0700

    adjust things a bit

--- src/pages/SvgTest.js
+++ src/pages/SvgTest.js
@@ -6,106 +6,108 @@ const SvgTest = () => {
   let container;
 
   onMount(() => {
-    const scene = new THREE.Scene();
-    
-
     const width = window.innerWidth;
     const height = window.innerHeight;
     const aspectRatio = width / height;
-
     const viewSize = 200;
 
-    const camera = new THREE.OrthographicCamera(
-      (viewSize * aspectRatio) / -2,
-      (viewSize * aspectRatio) / 2,
-      viewSize / 2,
-      viewSize / -2,
-      1,
-      1000
-    );
+    const backgroundColor = "#656565";
+    const rectColor = "#80b4ad";
 
-    camera.position.set(0, 0, 200);
-    camera.lookAt(0, 0, 0);
-
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    container.appendChild(renderer.domElement);
-
-    const rectSize = 300;
+    const rectSize = 256;
     const borderRadius = 30;
     const paddingSize = 50;
     const comb = rectSize + paddingSize;
-    const gridSize = 10;
+    const rectGridSize = 10;
 
-    const backgroundColor = "#656565";
-    const rectColor = "#89c1cd";
-
-    scene.background = new THREE.Color(`${backgroundColor}`);
+    const planeSize = 1;
+    const planeGridSize = 1;
 
     const svgString = `
-    <svg width="${comb * gridSize}" height="${
-      comb * gridSize
+    <svg width="${comb * rectGridSize}" height="${
+      comb * rectGridSize
     }" xmlns="http://www.w3.org/2000/svg">
-    <!-- Padding around the entire SVG -->
-    <rect width="100%" height="100%" fill="${backgroundColor}" />
-  
-    <defs>
-      <!-- Inner shadow filter definition -->
-      <filter id="inner-shadow" x="-50%" y="-50%" width="200%" height="200%">
-        <feComponentTransfer in="SourceAlpha">
-          <feFuncA type="table" tableValues="1 0" />
-        </feComponentTransfer>
-        <feGaussianBlur stdDeviation="${40}" />
-        <feOffset dx="${-20}" dy="${50}" result="offsetblur" />
-        <feFlood flood-color="#000000" result="color" />
-        <feComposite in2="offsetblur" operator="in" />
-        <feComposite in2="SourceAlpha" operator="in" />
-        <feMerge>
-          <feMergeNode in="SourceGraphic" />
-          <feMergeNode />
-        </feMerge>
-      </filter>
+      <rect width="100%" height="100%" fill="${backgroundColor}" />
 
-      <filter id="pop-out-effect" x="-50%" y="-50%" width="200%" height="200%">
-        <!-- Apply a feConvolveMatrix filter to create a beveled edge -->
-        <feConvolveMatrix
-          in="SourceGraphic"
-          result="convolved"
-          order="3"
-          kernelMatrix="0 -1 0 -1 5 -1 0 -1 0"
-        />
-        <!-- Apply a feComposite filter to combine the original and beveled shape -->
-        <feComposite in="convolved" in2="SourceGraphic" operator="arithmetic" k1="0" k2="1" k3="1" k4="0" />
-      </filter>
-  
-      <rect id="rounded-rect" width="${rectSize}" height="${rectSize}" rx="${borderRadius}" ry="${borderRadius}" style="fill:${rectColor}; stroke: white; filter:url(#inner-shadow);" />
-    </defs>
-  
-    <g fill="none" stroke="white">
-      ${Array.from(
-        { length: gridSize },
-        (_, rowIndex) =>
-          `
-            ${Array.from(
-              { length: gridSize },
-              (_, colIndex) =>
-                `<use href="#rounded-rect" x="${
-                  colIndex * (rectSize + paddingSize) + paddingSize / 2
-                }" y="${
-                  rowIndex * (rectSize + paddingSize) + paddingSize / 2
-                }" />`
-            ).join("\n       ")}`
-      ).join("\n    ")}
-    </g>
-  </svg>
-  
+      <defs>
+        <filter id="inner-shadow" x="-50%" y="-50%" width="200%" height="200%">
+          <feComponentTransfer in="SourceAlpha">
+            <feFuncA type="table" tableValues="1 0" />
+          </feComponentTransfer>
+          <feGaussianBlur stdDeviation="${40}" />
+          <feOffset dx="${-20}" dy="${50}" result="offsetblur" />
+          <feFlood flood-color="#000000" result="color" />
+          <feComposite in2="offsetblur" operator="in" />
+          <feComposite in2="SourceAlpha" operator="in" />
+          <feMerge>
+            <feMergeNode in="SourceGraphic" />
+            <feMergeNode />
+          </feMerge>
+        </filter>
 
+        <filter id="pop-out-effect" x="-50%" y="-50%" width="200%" height="200%">
+          <feConvolveMatrix
+            in="SourceGraphic"
+            result="convolved"
+            order="3"
+            kernelMatrix="0 -1 0 -1 5 -1 0 -1 0"
+          />
+          <feComposite in="convolved" in2="SourceGraphic" operator="arithmetic" k1="0" k2="1" k3="1" k4="0" />
+        </filter>
+
+        <rect id="rounded-rect" width="${rectSize}" height="${rectSize}" rx="${borderRadius}" ry="${borderRadius}" fill="${rectColor}" stroke="#d4d4d4" stroke-width="10" style="filter:url(#inner-shadow);" />
+      </defs>
+
+      <g fill="none" stroke="white">
+        ${Array.from(
+          { length: rectGridSize },
+          (_, rowIndex) =>
+            `
+              ${Array.from(
+                { length: rectGridSize },
+                (_, colIndex) =>
+                  `<use href="#rounded-rect" x="${
+                    colIndex * (rectSize + paddingSize) + paddingSize / 2
+                  }" y="${
+                    rowIndex * (rectSize + paddingSize) + paddingSize / 2
+                  }" />`
+              ).join("\n       ")}`
+        ).join("\n    ")}
+      </g>
+    </svg>
     `;
 
     const svgBlob = new Blob([svgString], {
       type: "image/svg+xml;charset=utf-8",
     });
     const svgUrl = URL.createObjectURL(svgBlob);
+
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(`${backgroundColor}`);
+
+    const camera = new THREE.OrthographicCamera(
+      (viewSize * aspectRatio) / -2,
+      (viewSize * aspectRatio) / 2,
+      viewSize / 2,
+      viewSize / -2
+    );
+
+    camera.position.set(0, 0, 5);
+    camera.lookAt(0, 0, 0);
+    camera.zoom = 70;
+    camera.updateProjectionMatrix();
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+
+    const updateRendererSize = () => {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+    };
+
+    window.addEventListener("resize", updateRendererSize);
 
     const loader = new THREE.TextureLoader();
     loader.load(svgUrl, function (texture) {
@@ -118,11 +120,8 @@ const SvgTest = () => {
         transparent: true,
       });
 
-      const planeSize = 1;
-      const gridSize = 100;
-
-      for (let row = 0; row < gridSize; row++) {
-        for (let col = 0; col < gridSize; col++) {
+      for (let row = 0; row < planeGridSize; row++) {
+        for (let col = 0; col < planeGridSize; col++) {
           const mesh = new THREE.Mesh(
             new THREE.PlaneGeometry(planeSize, planeSize),
             material
@@ -133,10 +132,6 @@ const SvgTest = () => {
           scene.add(mesh);
         }
       }
-
-      camera.position.z = 5;
-      camera.zoom = 70;
-      camera.updateProjectionMatrix();
 
       let isPanning = false;
 
@@ -195,17 +190,13 @@ const SvgTest = () => {
         event.preventDefault();
 
         const delta = -event.deltaY / 3;
-
         camera.zoom += delta * zoomSpeed;
         camera.zoom = Math.max(1, camera.zoom);
-
         camera.updateProjectionMatrix();
-
         requestAnimationFrame(render);
       };
 
       container.addEventListener("wheel", onMouseWheel);
-      
 
       render();
     });
